2a2178f312730cc8d41e4fd297e6bef8d0f7a0a5
Vasil Raychev <vasil_raychev@students.softuni.bg>
2023-05-09 20:05:42 +0300

    Recreate Class Grades

--- School_Diary/School_Diary/Data/Models/Grade.cs
+++ School_Diary/School_Diary/Data/Models/Grade.cs
@@ -1,85 +1,86 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-namespace School_Diary.Data.Models;
-
-public class Grade : IComparable<Grade>
+namespace School_Diary.Data.Models
 {
-    private int gradeNumber;
-    private string gradeName;
-
-    public int GradeId { get; set; }
-
-    public int GradeNumber
+    public class Grade : IComparable<Grade>
     {
-        get
-        {
-            return this.gradeNumber;
-        }
-        set
-        {
-            if (value < 1)
-            {
-                throw new ArgumentException("The grade should be at least 1!");
-            }
-            if (value > 12)
-            {
-                throw new ArgumentException("The grade should be at maximum 12!");
-            }
-            this.gradeNumber = value;
-        }
-    }
+        private int gradeNumber;
+        private string gradeName;
 
-    public string GradeName
-    {
-        get
+        public int GradeId { get; set; }
+
+        public int GradeNumber
         {
-            return this.gradeName;
-        }
-        set
-        {
-            if (string.IsNullOrEmpty(value))
+            get
             {
-                throw new ArgumentException("The grade name cannot be empty!");
+                return this.gradeNumber;
             }
-            if (!char.IsUpper(value[0]))
+            set
             {
-                throw new ArgumentException("The first letter on grade name cannot be lower!");
-            }
-            if (value.Length < 1)
-            {
-                throw new ArgumentException("The grade name is too short! It should be at least 1 letter.");
-            }
-            if (value.Length > 20)
-            {
-                throw new ArgumentException("Тhe grade name is too long! It should be at maximum 20 letter.");
-            }
-            for (int i = 0; i < value.Length; i++)
-            {
-                if (!char.IsLetter(value[i]) && value[i] != ' ')
+                if (value < 1)
                 {
-                    throw new ArgumentException("The grade name should be in letters only!");
+                    throw new ArgumentException("The lowest grade is 1!");
                 }
+                if (value > 12)
+                {
+                    throw new ArgumentException("The highest grade is 12!");
+                }
+                this.gradeNumber = value;
             }
-            this.gradeName = value;
         }
-    }
 
-    public bool IsDelete { get; set; }
-
-    public virtual ICollection<Student> Students { get; } = new List<Student>();
-
-    public string PrintGrade()
-    {
-        return $"{this.GradeNumber} {this.GradeName}";
-    }
-
-    public int CompareTo([AllowNull] Grade other)
-    {
-        int result = this.GradeNumber.CompareTo(other.GradeNumber);
-        if (result == 0)
+        public string GradeName
         {
-            result = this.GradeName.CompareTo(other.GradeName);
+            get
+            {
+                return this.gradeName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Grade name cannot be empty!");
+                }
+                if (!char.IsUpper(value[0]))
+                {
+                    throw new ArgumentException("The first letter of grade name cannot be lower!");
+                }
+                if (value.Length < 1)
+                {
+                    throw new ArgumentException("Grade name is too short! It should be at least 1 letter.");
+                }
+                if (value.Length > 20)
+                {
+                    throw new ArgumentException("Grade name is too long! It should be a maximum of 20 letters.");
+                }
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (!char.IsLetter(value[i]) && value[i] != ' ')
+                    {
+                        throw new ArgumentException("Grade name should be in letters only!");
+                    }
+                }
+                this.gradeName = value;
+            }
         }
-        return result;
+
+        public bool IsDelete { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; } = new List<Student>();
+
+        public string PrintGrade()
+        {
+            return $"{this.GradeNumber} {this.GradeName}";
+        }
+
+        public int CompareTo([AllowNull] Grade other)
+        {
+            int result = this.GradeNumber.CompareTo(other.GradeNumber);
+            if (result == 0)
+            {
+                result = this.GradeName.CompareTo(other.GradeName);
+            }
+            return result;
+        }
     }
 }
